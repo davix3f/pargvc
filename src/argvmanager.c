6f177ae7f945385fab2d21ea7219d97a41e0edff
Davide <davide_fiorito@libero.it>
2018-07-29 00:04:56 +0200

    Added basic functionalities
    Instruction in and description in header file.
    Also, names check out

--- src/argvmanager.c
+++ src/argvmanager.c
@@ -1,14 +1,25 @@
 #include "argvmanager.h"
 
+void argvmanager_init(int number_of_args)
+{
+	arg_count = number_of_args;
+	args_list = NULL;
+	args_list = calloc(number_of_args, sizeof(cl_argument));
+}
+
 
 int add_argument(const char* flag, const char* extended, void (*function_to_point()))
 {
+	printf("%i\n", arg_count);
 	printf("Created command %s [extended version: %s] for function %x\n", flag, extended, function_to_point);
 	
-	cl_argument newarg={flag, extended, function_to_point};
+	cl_argument newarg = {flag, extended, function_to_point};
+
+	append_to_args(newarg);
 
 	return 0;
 }
+
 
 int remove_argument(const char* flag)
 {
@@ -16,3 +27,50 @@ int remove_argument(const char* flag)
 
 	return 0;
 }
+
+
+int append_to_args(cl_argument argument)
+{
+    int index=0;
+	for(int x=0; x<arg_count; x++)
+	{
+		if(args_list[x].flag==NULL)
+    	{
+    		printf("Assigned %x to args[%i](%x)\n", &argument, index, &args_list[index]);
+    		args_list[x] = argument;
+    		return 1;
+    	}
+    }
+}
+
+
+int check_flag(const char* flag_to_check, char** argv_c, int args_count)
+{
+	for(int x=0; x<args_count; x++)
+	{
+		if((strcmp(args_list[x].flag, flag_to_check)==0) || strcmp(args_list[x].extended, flag_to_check)==0)
+		{
+			printf("Flag %s exists at %i\n", flag_to_check, x);
+			for(int index=0; index<	args_count; index++)
+			{
+				if((strcmp(flag_to_check, argv_c[index])==0) || strcmp(args_list[x].extended, argv_c[index])==0)
+				{
+					return 1;
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+
+void get_all_args()
+{
+	int index=0;
+	while(args_list[index].flag!=NULL)
+	{
+		printf("[%i] #%i (Flag: %s, Expanded: %s, Function: %x)\n", index, index+1, args_list[index].flag, args_list[index].extended, args_list[index].function_to_call);
+		index++;
+	}
+}
+
--- src/argvmanager.h
+++ src/argvmanager.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct clarg
 {
@@ -7,9 +8,25 @@ typedef struct clarg
 	void (*function_to_call)(char* function_argument);
 } cl_argument;
 
-cl_argument* arglist[];
+static cl_argument *args_list;
+static int arg_count;
+
+//Launch this with argc as argument before anything else
+void argvmanager_init(int number_of_args);
 
 int add_argument(const char* flag, const char* extended, void (*function_to_point()));
 
 int remove_argument(const char* flag);
+
+int append_to_args(cl_argument argument); // not for user
+
+/* Use that in your program to check if an argument has been given
+e.g "if (check_flag("-a", argv, argc)){do_this;} "
+also supports extended flags
+e.g "if(check_flag("--extended-version", argv, argc)){do_that;}"
+*/
+int check_flag(const char* flag_to_check, char** argv_c, int args_count);
+
+//print all args
+void get_all_args();
 
