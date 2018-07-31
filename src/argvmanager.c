8743547221e093319674160d49e7d085c6b61521
Davide <davide_fiorito@libero.it>
2018-07-31 20:20:35 +0200

    check_flag now supports extended command version

--- src/argvmanager.c
+++ src/argvmanager.c
@@ -70,12 +70,12 @@ int check_flag(const char* flag_to_check)
 {
 	for(int x=0; x<optional_args_count-1; x++)
 	{
-		if((strcmp(optional_args_list[x].flag, flag_to_check)==0) || strcmp(optional_args_list[x].extended, flag_to_check)==0)
+		if(strcmp(optional_args_list[x].flag, flag_to_check)==0 || strcmp(optional_args_list[x].extended, flag_to_check)==0)
 		{ //check if the flag to be searched exists in the flags array
 		  //if it exists
 			for(int index=0; index<argc_copy; index++)
 			{ // argv is parsed to look for it
-				if((strcmp(flag_to_check, argv_copy[index])==0) || strcmp(optional_args_list[x].extended, argv_copy[index])==0)
+				if(strcmp(optional_args_list[x].flag, argv_copy[index])==0 || strcmp(optional_args_list[x].extended, argv_copy[index])==0)
 				{
 					return 1;
 				}
