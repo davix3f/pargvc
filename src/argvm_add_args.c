e67323521399ee51ffa0f98118aa7f4ad83e84b8
davix3f <davide_fiorito@libero.it>
2018-08-26 22:53:00 +0200

    Added support for the new iarg struct version

--- src/argvm_add_args.c
+++ src/argvm_add_args.c
@@ -2,8 +2,6 @@
 
 int add_optional_argument(const char* flag, const char* extended, _Bool has_value, const char* help)
 {
-	//printf("Created optional argument \'%s\' [extended version: \'%s\']\n", flag, extended);
-
 	optional_cl_argument new_optional_arg = {flag, extended, has_value, help};
 
 	append_to_oargs(new_optional_arg);
@@ -15,9 +13,9 @@ int add_mandatory_argument(const char* flag, const char* help)
 {
 	if(strncmp(flag, "-", 1)==0)
 	{
-		printf("Mandatory arguments should not be named with \'--\'or \'-\' at the beginning\n");
+		printf("Mandatory arguments cannot not be named with \'--\'or \'-\' at the beginning\n");
+		return 0;
 	}
-	//printf("Created mandatory argument \'%s\'\n", flag);
 	mandatory_cl_argument new_mandatory = {flag, help};
 	append_to_margs(new_mandatory);
 	return 1;
@@ -50,24 +48,27 @@ int append_to_margs(mandatory_cl_argument argument)
     return 0;
 }
 
-int add_input_value(const char* flag, const char* input_val)
+int append_input_value(const char* flag, const char* input_val, int is_mandatory_val)
 {
 	for(int x=0; x<argc_copy; x++)
 	{
 		if(input_args[x].name==NULL)
     	{
-			argv_value arg = {flag, input_val};
+			argv_value arg = {flag, input_val, is_mandatory_val};
     		input_args[x] = arg;
-			//printf("added %s with val %s\n", arg.name, arg.value);
     		return 1;
     	}
     }
 	return 0;
 }
 
-int add_mandatory_value(const char* input_val)
+int append_mandatory_value(const char* input_val)
 {
 	int found = 0;
+	/*
+	checking if the input_args value
+	exists in mandatory args list   
+	*/
 	for(int x=0; x<mandatory_args_count; x++)
 	{
 		for(int y=0; y<argc_copy; y++)
@@ -81,7 +82,7 @@ int add_mandatory_value(const char* input_val)
 		if(found == 0)
 		{
 			//printf("\nAdding val[%s] to marg[%s]\n", input_val, mandatory_args_list[x].flag);
-			add_input_value(mandatory_args_list[x].flag, input_val);
+			add_input_value(mandatory_args_list[x].flag, input_val, 1);
 			return 0;
 		}
 
