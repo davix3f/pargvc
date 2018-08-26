846605eff32ee9b8b39ed9d6418fb73627daf98a
davix3f <davide_fiorito@libero.it>
2018-08-26 22:54:59 +0200

    iarg struct updated; declaration layout updated with sections and comments

--- src/argvmanager.h
+++ src/argvmanager.h
@@ -20,6 +20,7 @@ typedef struct i_arg
 {
 	const char* name;
 	const char* value;
+	int is_mandatory;
 } argv_value;
 
 optional_cl_argument *optional_args_list;
@@ -30,33 +31,39 @@ int argc_copy;
 int optional_args_count;
 int mandatory_args_count;
 
-//Launch this with argc as argument before anything else
+// Call this with before anything else
 void argvmanager_init(int number_of_mandatory_args, int number_of_optional_args, char** argv, int argc);
+
+// Call this at the end of the function
 void argvmanager_end();
 
-int add_optional_argument(const char* flag, const char* extended, _Bool has_value, const char* help);
-int add_mandatory_argument(const char* flag, const char* help);
-int add_mandatory_value(const char* input_val);
-int add_input_value(const char* flag, const char* input_val);
 
-
+// Backends
+// NOT FOR USER
+// These functions shouldn't be called by the user.
+int append_mandatory_value(const char* input_val); // not for user
+int append_input_value(const char* flag, const char* input_val, int is_mandatory); // not for user
 int append_to_oargs(optional_cl_argument argument); // not for user
 int append_to_margs(mandatory_cl_argument argument); //not for user
+int help_flag();
+
+// Frontends
+// FOR USER
+// Call those functions to interact with argv values
+int add_optional_argument(const char* flag, const char* extended, _Bool has_value, const char* help);
+int add_mandatory_argument(const char* flag, const char* help);
+
+void parse_argv();
 
 /* Use that in your program to check if an argument has been given
 e.g "if (check_flag("-a", argv, argc)){do_this;} "
 also supports extended flags
 e.g "if(check_flag("--extended-version", argv, argc)){do_that;}"
 */
-void parse_argv();
 int check_flag(const char* flag_to_check);
 
 //print all args
 void get_all_args();
+
+// get the value of a flag
 const char* get_arg_value(const char* arg_name);
-int help_flag(); // execute this function at the end of the arguments declaration or of the whole script
-/*
-add_mandatory_argument(this this)
-add_optional_argument(this this this)
-add_optional_argument(that those these)
-help_flag()*/
